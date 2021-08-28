337411dd8628a5840a5b3985af210879d66a55cc
Leon Rode <leon.rode13@gmail.com>
2021-08-28 00:13:31 -0400

    Add files via upload

--- components/about.js
+++ components/about.js
@@ -8,12 +8,12 @@ export default function About({ slideState }) {
       <p className={styles.text}>
         <span className={styles.bold}>Hi there!</span> I&apos;m Leon Rode, a
         high-school student, software developer, and freelancer based in New
-        York. I&apos;ve been programming for about seven years now, and
-        it&apos;s still my favorite passion. I taught myself what I know, and
-        I&apos;ve used my skills to develop a variety of software, including
-        websites, Arduino drivers, and other utilities, some of which are listed
-        further down. Aside from developing, I like to play chess, video games,
-        and listen to music.
+        York. I have been programming for over seven years, and it has always
+        been something I am truly passionate about. I am fully self-taught, and
+        I have used my skills to develop a variety of software, including
+        websites, Arduino drivers, and other utilities (some of which are listed
+        further down). Aside from developing, I enjoy playing chess, video
+        games, and piano.
       </p>
     </div>
   );
